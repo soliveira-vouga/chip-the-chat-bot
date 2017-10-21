a2acead915b09cedfebd34e951f8eff7c334fad1
Steve Oliveira <steve@oliveira.io>
2017-10-21 17:43:35 -0400

    Updated token environment variable

--- index.js
+++ index.js
@@ -39,13 +39,13 @@ app.post('/webhook/', function (req, res) {
     res.sendStatus(200)
 })
 
-const token = process.env.FB_PAGE_ACCESS_
+const token = process.env.FB_PAGE_ACCESS_TOKEN
 
 function sendTextMessage(sender, text) {
     let messageData = { text:text }
     request({
 	    url: 'https://graph.facebook.com/v2.6/me/messages',
-	    qs: {access_token:token},
+	    qs: { access_token: token },
 	    method: 'POST',
 		json: {
 		    recipient: {id:sender},
