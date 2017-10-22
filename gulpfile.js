8f6a86bfedecd6b92113682cfd8335fefb6cfef0
Steve Oliveira <steve@oliveira.io>
2017-10-21 20:40:07 -0400

    Fixed linter and linting

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const gulp = require('gulp')
 const standard = require('gulp-standard')
 
 gulp.task('lint', function () {
-  return gulp.src(['./**.js'])
+  return gulp.src(['./*.js', './lib/**/*.js'])
     .pipe(standard())
     .pipe(standard.reporter('default', {
       breakOnError: true
